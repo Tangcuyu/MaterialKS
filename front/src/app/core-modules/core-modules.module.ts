dfeb41031393acc38b0f4353d97c37df28fa7086
tangcuyu <joysmshr@126.com>
2018-11-02 18:07:00 +0800

    upgrade to angular7

--- front/src/app/core-modules/core-modules.module.ts
+++ front/src/app/core-modules/core-modules.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserServiceService } from './user-service.service';
 import { AuthGuardService } from './auth-guard.service';
@@ -20,4 +20,21 @@ import { NotifyService } from './notify.service';
   ],
   exports: [CommonModule]
 })
-export class CoreModulesModule { }
+export class CoreModulesModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModulesModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+
+/* 可以在forRoot静态方法中，配置CoreModule模块中的服务提供商
+  static forRoot(config: UserServiceConfig): ModuleWithProviders {
+    return {
+      ngModule: CoreModule,
+      providers: [
+        { provide: UserServiceConfig, useValue: config }
+      ]
+    };
+  } */
+}
